3d1dc38aedb13f80f60c8435465e02e6d6092274
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-08 20:13:11 +0200

    Aggiunto js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(
   function(){
 
+      $(".fa-paper-plane").click(
+        function(){
+          var messaggio = $("#myMessage").val();
+          var elemento = $(".template").clone();
+          elemento.prepend(messaggio);
+          elemento.removeClass("template");
+          elemento.addClass("msg");
+          $(".lista-messaggi").append(elemento);
+          $("#myMessage").val("");
+        }
+      );
 
 
 
 
-    
   }
 );
