e2498a658bacb3eb40989c4730c2486e04c28a3d
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-09 17:23:19 +0200

    Cambiato codice html css e js e aggiunta autorisposta CPU

--- js/main.js
+++ js/main.js
@@ -1,20 +1,58 @@
-$(document).ready(
-  function(){
+$(document).ready(function() {
 
-      $(".fa-paper-plane").click(
-        function(){
-          var messaggio = $("#myMessage").val();
-          var elemento = $(".template").clone();
-          elemento.prepend(messaggio);
-          elemento.removeClass("template");
-          elemento.addClass("msg");
-          $(".lista-messaggi").append(elemento);
-          $("#myMessage").val("");
-        }
-      );
+  $(".send-message").click(
+    function() {
+      sendMessage();
+      setTimeout(rispostaCpu,1000);
+
+    }
+  );
+
+  $("#input-message").keyup(
+    function(event) {
+      if(event.which == 13) {
+        sendMessage();
+        setTimeout(rispostaCpu,1000);
 
 
+      }
+    }
+  );
 
 
+});
+
+function sendMessage() {
+  var inputText = $("#input-message").val();
+
+  if(inputText != "") {
+    var templateMessage = $(".templates .message-row").clone();
+
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var time = hours + ":" + minutes;
+
+    templateMessage.find(".message-text").text(inputText);
+    templateMessage.find(".message-time").text(time);
+    templateMessage.addClass("sent");
+
+    $(".chat").append(templateMessage);
+    $("#input-message").val("");
   }
-);
+}
+
+function rispostaCpu(){
+  var templateMessageCpu = $(".templates .message-row").clone();
+
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var time = hours + ":" + minutes;
+
+  templateMessageCpu.find(".message-time").text(time);
+  templateMessageCpu.addClass("ricevuta");
+
+  templateMessageCpu.find(".message-text").text("ok");
+  $(".chat").append(templateMessageCpu);
+}
